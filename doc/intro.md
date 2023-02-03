d97615f7c65234f11c5ca670fc9d1f557caea611
mes32 <stockman.mike@gmail.com>
2023-02-02 23:57:08 -0600

    run: lein new app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stoic-garden-59244
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/stoic_garden_59244/core.clj
@@ -0,0 +1,7 @@
+(ns stoic-garden-59244.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
