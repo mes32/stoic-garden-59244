77510c5a8bbec3653a789e4439737e999b444e6e
mes32 <stockman.mike@gmail.com>
2023-02-03 00:37:50 -0600

    get the splash page to render

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]]
-  :main ^:skip-aot stoic-garden-59244.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [environ "1.1.0"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "stoic-garden-59244.jar"
+  :profiles {:production {:env {:production true}}})
